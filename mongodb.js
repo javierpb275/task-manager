264317bfc6de8a61f44e5e0882edfc5042d0438a
javierpb275 <javierpb275@gmail.com>
2021-07-13 19:21:02 +0200

    created database and collection and inserted document

--- mongodb.js
+++ mongodb.js
@@ -10,5 +10,12 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client) => {
     if (error) {
         return console.log('Unable to connect to database');
     }   
-    console.log('Connected correctly');
+    
+    const db = client.db(databaseName);
+    
+    db.collection('users').insertOne({
+        name: 'Pepe',
+        age: 30
+    });
+
 });
