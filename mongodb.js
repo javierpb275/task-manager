939f502ec670c646b45f7fab6c83c1687b61757d
javierpb275 <javierpb275@gmail.com>
2021-07-13 19:39:03 +0200

    inserting many documents in user's collection with insertMany()

--- mongodb.js
+++ mongodb.js
@@ -1,18 +1,21 @@
 //CRUD create read update delete
 
-const mongodb = require('mongodb');
+const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
 
-const connectionURL = 'mongodb://127.0.0.1:27017';//or 'mongodb://localhost:27017'
-const databaseName = 'task-manager';
+const connectionURL = "mongodb://127.0.0.1:27017"; //or 'mongodb://localhost:27017'
+const databaseName = "task-manager";
 
-MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client) => {
+MongoClient.connect(
+  connectionURL,
+  { useNewUrlParser: true },
+  (error, client) => {
     if (error) {
-        return console.log('Unable to connect to database');
-    }   
-    
-    const db = client.db(databaseName);
+      return console.log("Unable to connect to database");
+    }
 
+    const db = client.db(databaseName);
+    /*
     db.collection('users').insertOne({
         name: 'Pepe',
         age: 30
@@ -22,5 +25,25 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client) => {
         }
         console.log(result.ops)//all the documents inserted
     });
+    */
 
-});
+    db.collection("users").insertMany(
+      [
+        {
+          name: "Paco",
+          age: 40,
+        },
+        {
+          name: "Maria",
+          age: 20,
+        },
+      ],
+      (error, result) => {
+        if (error) {
+          return console.log("Unable to insert users");
+        }
+        console.log(result.ops); //all the documents inserted
+      }
+    );
+  }
+);
