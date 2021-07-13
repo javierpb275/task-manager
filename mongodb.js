e2d3371b3d33d993678586d27d9ef101986ea838
javierpb275 <javierpb275@gmail.com>
2021-07-13 19:12:44 +0200

    connecting and checking connection to mongodb database

---
+++ mongodb.js
@@ -0,0 +1,14 @@
+//CRUD create read update delete
+
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+const connectionURL = 'mongodb://127.0.0.1:27017';//or 'mongodb://localhost:27017'
+const databaseName = 'task-manager';
+
+MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client) => {
+    if (error) {
+        return console.log('Unable to connect to database');
+    }   
+    console.log('Connected correctly');
+});
