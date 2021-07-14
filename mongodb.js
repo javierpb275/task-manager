ee710a21bb0fae370011cd5acaa25b77c2ae824b
javierpb275 <javierpb275@gmail.com>
2021-07-14 19:56:44 +0200

    removed comments objectid code and insert data code.

--- mongodb.js
+++ mongodb.js
@@ -1,19 +1,9 @@
 //CRUD create read update delete
 
-/*const mongodb = require("mongodb");
-const MongoClient = mongodb.MongoClient;
-const ObjectID = mongodb.ObjectID;*/
-
-const { MongoClient, ObjectID } = require('mongodb');
+const { MongoClient } = require('mongodb');
 
 const connectionURL = "mongodb://127.0.0.1:27017"; //or 'mongodb://localhost:27017'
 const databaseName = "task-manager";
-
-const id = new ObjectID();
-console.log(id);
-console.log(id.getTimestamp());
-console.log(id.id.length);
-console.log(id.toHexString().length);
 
 MongoClient.connect(
   connectionURL,
@@ -25,59 +15,5 @@ MongoClient.connect(
 
     const db = client.db(databaseName);
 
- /*   db.collection("tasks").insertMany(
-      [
-        {
-          description: "make the bed",
-          completed: true,
-        },
-        {
-          description: "do the shopping",
-          completed: true,
-        },
-        {
-          description: "make dinner",
-          completed: false,
-        },
-      ],
-      (error, result) => {
-        if (error) {
-          return console.log("Unable to insert tasks");
-        }
-        console.log(result.ops);
-      }
-    );*/
-
-    db.collection('users').insertOne({
-        _id: id,
-        name: 'Paquito',
-        age: 21
-    }, (error, result) => {
-        if (error) {
-            return console.log('Unable to insert user');
-        }
-        console.log(result.ops)//all the documents inserted
-    });
-    
-/*
-    db.collection("users").insertMany(
-      [
-        {
-          name: "Paco",
-          age: 40,
-        },
-        {
-          name: "Maria",
-          age: 20,
-        },
-      ],
-      (error, result) => {
-        if (error) {
-          return console.log("Unable to insert users");
-        }
-        console.log(result.ops); //all the documents inserted
-      }
-    );
-    */
   }
 );
