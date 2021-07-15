f9428fc889a0d06a064baf44c140b6976122f5e3
javierpb275 <javierpb275@gmail.com>
2021-07-15 19:49:15 +0200

    updating documents

--- mongodb.js
+++ mongodb.js
@@ -9,10 +9,41 @@ MongoClient.connect(
   connectionURL,
   { useNewUrlParser: true },
   (error, client) => {
+
     if (error) {
       return console.log("Unable to connect to database");
     }
 
     const db = client.db(databaseName);
+
+    const updatePromise = db.collection('users').updateOne({
+      _id: new ObjectID('60edcc7699b2ad1840e045d0')
+    }, {
+      /*$set: {
+        name: 'Mike'
+      }*/
+      $inc: {
+        age: 1
+      }
+    })
+
+    updatePromise.then(result => {
+      console.log(result);
+    }).catch(error => {
+      console.log(error);
+    });
+
+    db.collection('tasks').updateMany({
+      completed: true
+    }, {
+      $set: {
+        completed: false
+      }
+    }).then(result => {
+      console.log(result.modifiedCount);
+    }).catch(error => {
+      console.log(error);
+    })
+
   }
 );
