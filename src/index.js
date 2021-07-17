2bd0ad92ddd3aaaba3f6fe137aa20e1f6a96b88c
javierpb275 <javierpb275@gmail.com>
2021-07-17 17:17:58 +0200

    Reading endpoints: getting users and user by id

--- src/index.js
+++ src/index.js
@@ -17,6 +17,26 @@ app.post('/users', (req, res) => {
     })
 });
 
+app.get('/users', (req, res) => {
+    User.find({}).then(users => {
+        res.status(200).send(users);
+    }).catch(e => {
+        res.status(500).send(e);
+    })
+})
+
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id;
+    User.findById(_id).then(user => {
+        if (!user) {
+            return res.status(404).send()
+        }
+        res.status(200).send(user);
+    }).catch(e => {
+        res.status(404).send(e);
+    })
+})
+
 app.post('/tasks', (req, res) => {
     const task = new Task(req.body);
     task.save().then(() => {
