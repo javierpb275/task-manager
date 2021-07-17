1eae02e468f20e5a42ef00a5c9ff9652b2bfa1f0
javierpb275 <javierpb275@gmail.com>
2021-07-17 17:54:39 +0200

    reading endpoints: get task by id and get tasks

--- src/index.js
+++ src/index.js
@@ -33,7 +33,7 @@ app.get('/users/:id', (req, res) => {
         }
         res.status(200).send(user);
     }).catch(e => {
-        res.status(404).send(e);
+        res.status(500).send(e);
     })
 })
 
@@ -45,6 +45,26 @@ app.post('/tasks', (req, res) => {
         res.status(400).send(e);
     })
 });
+
+app.get('/tasks', (req, res) => {
+    Task.find({}).then(tasks => {
+        res.status(200).send(tasks);
+    }).catch(e => {
+        res.status(500).send(e);
+    })
+})
+
+app.get('/tasks/:id', (req, res) => {
+    const _id = req.params.id;
+    Task.findById(_id).then(task => {
+        if (!task) {
+            return res.status(404).send();
+        }
+        res.status(200).send(task);
+    }).catch(e => {
+        res.status(500).send(e);
+    })
+})
 
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`);
