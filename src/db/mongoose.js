7169dcca35fb17d7d36355829229ca760acadd62
javierpb275 <javierpb275@gmail.com>
2021-07-17 13:36:02 +0200

    in index.js: created endpoint for posting a new user and tested using postman by console log req.body send testing as response. used express.json to automatically parse json data. required mongoose.js file to connect to the database. in mongoose.js:  removed user model and creation of users and tasks. Created models folder and inside user.js model where we define the User model and export it.

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const validator = require('validator');
 
 const connectionURL = "mongodb://127.0.0.1:27017"
 const databaseName = "task-manager-api";
@@ -8,60 +7,6 @@ mongoose.connect(`${connectionURL}/${databaseName}`, {
     useNewUrlParser: true, 
     useCreateIndex: true
 });
-
-const User = mongoose.model('User', {
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error('Email is invalid');
-            }
-        }
-    },
-    password: {
-        type: String,
-        required: true,
-        trim: true,
-        minLength: 6,
-        validate(value) {
-            if (value.toLowerCase().includes('password')) {
-                throw new Error('Password cannot contain "password"');
-            }
-        }
-    },
-    age: {
-        type: Number,
-        default: 0,
-        validate(value) {
-            if(value < 0) {
-                throw new Error('Age must be a positive number');
-            }
-        }
-    }
-})
-
-
-const user1 = new User({
-    name: '  Paquito  ',
-    email: 'PAQUITO@gmail.com',
-    password: 'ewjfnwi324234'
-});
-
-/*
-user1.save().then(user => {
-    console.log(user);
-}).catch(error => {
-    console.log('Error!', error)
-})
-*/
 
 const Task = mongoose.model('Task', {
     description: {
@@ -74,16 +19,3 @@ const Task = mongoose.model('Task', {
         default: false
     }
 });
-
-
-const task1 = new Task({
-    description: '   do the laundry   ',
-});
-
-
-task1.save().then(task => {
-    console.log(task);
-}).catch(error => {
-    console.log('Error!', error);
-});
-
--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
+require('./db/mongoose');//load mongoose file
 const express = require('express');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.json());//automatically parse json to an object
+
+app.post('/users', (req, res) => {
+    console.log(req.body);
+    res.send('testing');
+});
+
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`);
-})
+});
