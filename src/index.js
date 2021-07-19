8e08c636c55d462ed391c4343d01c148390cd625
javierpb275 <javierpb275@gmail.com>
2021-07-19 20:51:15 +0200

    separate route files: moved all the routes for users to the useRouter and changed app for router to use router class instead of the express class for the methods post, get... Then we can use those routes because in index.js we have imported that useRouter and we do app.use(userRouter)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 require("./db/mongoose"); //load mongoose file
 const express = require("express");
-const User = require("./models/user");
 const Task = require("./models/task");
 const userRouter = require('./routers/user');
 
@@ -10,72 +9,6 @@ const port = process.env.PORT || 3000;
 app.use(express.json()); //automatically parse json to an object
 
 app.use(userRouter);
-
-app.post("/users", async (req, res) => {
-  const user = new User(req.body);
-  try {
-    await user.save();
-    res.status(201).send(user);
-  } catch (e) {
-    res.status(400).send(e);
-  }
-});
-
-app.get("/users", async (req, res) => {
-  try {
-    const users = await User.find({});
-    res.status(200).send(users);
-  } catch (e) {
-    res.status(500).send(e);
-  }
-});
-
-app.get("/users/:id", async (req, res) => {
-  const _id = req.params.id;
-  try {
-    const user = await User.findById(_id);
-    if (!user) {
-      return res.status(404).send();
-    }
-    res.status(200).send(user);
-  } catch (e) {
-    res.status(500).send(e);
-  }
-});
-
-app.patch("/users/:id", async (req, res) => {
-  const {params, body} = req;
-  //check if what we try to update is NOT one of the properties of user:
-  const updates = Object.keys(body);
-  const allowedUpdates = ['name', 'email', 'password', 'age'];
-  const isValidOperation = updates.every(update => allowedUpdates.includes(update))
-  if (!isValidOperation) {
-    return res.status(400).send({error: 'Invalid updates!'});
-  }
-  //-------------------------
-  try {
-    const user = await User.findByIdAndUpdate(params.id, body, {new: true, runValidators: true});
-    if (!user) {
-      return res.status(404).send();
-    }
-    res.status(200).send(user);
-  } catch(e) {
-    res.status(400).send(e);
-  }
-});
-
-app.delete("/users/:id", async (req, res) => {
-  const _id = req.params.id;
-  try {
-    const user = await User.findByIdAndDelete(_id);
-    if (!user) {
-      return res.status(404).send()
-    }
-    res.status(200).send(user);
-  } catch(e) {
-    res.status(500).send(e);
-  }
-});
 
 app.post("/tasks", async (req, res) => {
   const task = new Task(req.body);
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,8 +1,76 @@
 const express = require("express");
+const User = require("../models/user");
 const router = new express.Router();
 
-router.get('/test', (req, res) => {
-  res.send('This is from my other router');
-})
+router.post("/users", async (req, res) => {
+  const user = new User(req.body);
+  try {
+    await user.save();
+    res.status(201).send(user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+router.get("/users", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.status(200).send(users);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
+router.get("/users/:id", async (req, res) => {
+  const _id = req.params.id;
+  try {
+    const user = await User.findById(_id);
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.status(200).send(user);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
+router.patch("/users/:id", async (req, res) => {
+  const { params, body } = req;
+  //check if what we try to update is NOT one of the properties of user:
+  const updates = Object.keys(body);
+  const allowedUpdates = ["name", "email", "password", "age"];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
+  if (!isValidOperation) {
+    return res.status(400).send({ error: "Invalid updates!" });
+  }
+  //-------------------------
+  try {
+    const user = await User.findByIdAndUpdate(params.id, body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.status(200).send(user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+router.delete("/users/:id", async (req, res) => {
+  const _id = req.params.id;
+  try {
+    const user = await User.findByIdAndDelete(_id);
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.status(200).send(user);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
 
 module.exports = router;
