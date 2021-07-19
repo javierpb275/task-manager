728f76a190ed5e0a0810b0cd1ab4f3af1882ca24
javierpb275 <javierpb275@gmail.com>
2021-07-19 19:51:47 +0200

    created updating endpoints for user and task

--- src/index.js
+++ src/index.js
@@ -40,6 +40,27 @@ app.get("/users/:id", async (req, res) => {
   }
 });
 
+app.patch("/users/:id", async (req, res) => {
+  const {params, body} = req;
+  //check if what we try to update is NOT one of the properties of user:
+  const updates = Object.keys(body);
+  const allowedUpdates = ['name', 'email', 'password', 'age'];
+  const isValidOperation = updates.every(update => allowedUpdates.includes(update))
+  if (!isValidOperation) {
+    return res.status(400).send({error: 'Invalid updates!'});
+  }
+  //-------------------------
+  try {
+    const user = await User.findByIdAndUpdate(params.id, body, {new: true, runValidators: true});
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.status(200).send(user);
+  } catch(e) {
+    res.status(400).send(e);
+  }
+})
+
 app.post("/tasks", async (req, res) => {
   const task = new Task(req.body);
   try {
@@ -71,6 +92,27 @@ app.get("/tasks/:id", async (req, res) => {
     res.status(500).send(e);
   }
 });
+
+app.patch("/tasks/:id", async (req, res) => {
+  const {params, body} = req;
+    //check if what we try to update is NOT one of the properties of task:
+    const updates = Object.keys(body);
+    const allowedUpdates = ['completed', 'description'];
+    const isValidOperation = updates.every(update => allowedUpdates.includes(update))
+    if (!isValidOperation) {
+      return res.status(400).send({error: 'Invalid updates!'});
+    }
+    //------------------------
+  try {
+    const task = await Task.findByIdAndUpdate(params.id, body, {new: true, runValidators: true});
+    if (!task) {
+      return res.status(404).send();
+    }
+    res.status(200).send(task);
+  } catch(e) {
+    res.status(400).send(e);
+  }
+})
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
