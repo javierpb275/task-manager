d445cf2c7f50e27c944d4b5eb5d11aec482002d8
javierpb275 <javierpb275@gmail.com>
2021-07-22 17:27:43 +0200

    installed bcrypt for encrypting passwords. created function example for how to use bcrypt

--- src/index.js
+++ src/index.js
@@ -11,3 +11,20 @@ app.use(express.json(), userRouter, taskRouter);
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
+
+const bcrypt = require('bcryptjs');
+
+const myFunction = async () => {  
+  const password = 'Red123!'
+  const hashedPassword = await bcrypt.hash(password, 8);
+
+  console.log(password);
+  console.log(hashedPassword);
+
+  const isMatch = await bcrypt.compare(password, hashedPassword);
+
+  console.log(isMatch);
+
+}
+
+myFunction();
