bd114a096b56728b095a7f240a72b591593ac176
javierpb275 <javierpb275@gmail.com>
2021-07-22 17:42:23 +0200

    created userSchema to be able to use middlewares like pre('save') that runs before saving and call next at the end to stop the execution. we pass that schema to the User model.

--- src/models/user.js
+++ src/models/user.js
@@ -1,43 +1,55 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 
-const User = mongoose.model('User', {
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error('Email is invalid');
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            trim: true,
+            lowercase: true,
+            validate(value) {
+                if (!validator.isEmail(value)) {
+                    throw new Error('Email is invalid');
+                }
             }
-        }
-    },
-    password: {
-        type: String,
-        required: true,
-        trim: true,
-        minLength: 6,
-        validate(value) {
-            if (value.toLowerCase().includes('password')) {
-                throw new Error('Password cannot contain "password"');
+        },
+        password: {
+            type: String,
+            required: true,
+            trim: true,
+            minLength: 6,
+            validate(value) {
+                if (value.toLowerCase().includes('password')) {
+                    throw new Error('Password cannot contain "password"');
+                }
             }
-        }
-    },
-    age: {
-        type: Number,
-        default: 0,
-        validate(value) {
-            if(value < 0) {
-                throw new Error('Age must be a positive number');
+        },
+        age: {
+            type: Number,
+            default: 0,
+            validate(value) {
+                if(value < 0) {
+                    throw new Error('Age must be a positive number');
+                }
             }
         }
     }
+);
+
+userSchema.pre('save', async function (next) {
+    const user = this;
+
+    console.log('just before saving');
+
+    next();
 })
+
+const User = mongoose.model('User', userSchema);
 
 module.exports = User;
