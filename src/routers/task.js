2cf4749903a96f3f2643f20b1dcb52e0934fe1ce
javierpb275 <javierpb275@gmail.com>
2021-07-22 18:38:13 +0200

    changed update of task to use findbyid() and method save() like with user

--- src/routers/task.js
+++ src/routers/task.js
@@ -47,10 +47,9 @@ router.patch("/tasks/:id", async (req, res) => {
   }
   //------------------------
   try {
-    const task = await Task.findByIdAndUpdate(params.id, body, {
-      new: true,
-      runValidators: true,
-    });
+    const task = await Task.findById(params.id);
+    updates.forEach(update => task[update] = body[update]);
+    await task.save();
     if (!task) {
       return res.status(404).send();
     }
