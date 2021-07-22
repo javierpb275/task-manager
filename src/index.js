7038c7f4f624baab83a60dc7c2d2a29001e8c50e
javierpb275 <javierpb275@gmail.com>
2021-07-22 19:56:23 +0200

    installed and checked jsonwebtoken with an example.

--- src/index.js
+++ src/index.js
@@ -11,3 +11,16 @@ app.use(express.json(), userRouter, taskRouter);
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
+
+const jwt = require('jsonwebtoken');
+
+const myFunction = async () => {
+  //the aim of jwt is to create data (id) that is verifiable via this signature: 'thisisnodejs'
+  const token = jwt.sign({_id: 'abc123'}, 'thisisnodejs', {expiresIn: '7 days'});//expiresIn: the token will expire when that time has passed.
+  console.log(token);
+  //to make sure that the user is authenticated correctly:
+  const data = jwt.verify(token, 'thisisnodejs');
+  console.log(data);
+}
+
+myFunction();
