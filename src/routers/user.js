7889beba5361f025999381b5e0a9c446e1946f4e
javierpb275 <javierpb275@gmail.com>
2021-07-31 18:37:37 +0200

    created /users/logoutAll route which removes all tokens sessions.

--- src/routers/user.js
+++ src/routers/user.js
@@ -37,6 +37,16 @@ router.post('/users/logout', auth, async (req, res) => {
   }
 })
 
+router.post('/users/logoutAll', auth, async (req, res) => {
+  try {
+    req.user.tokens = [];
+    await req.user.save();
+    res.send();
+  } catch (e) {
+    res.status(500).send();
+  }
+})
+
 router.get("/users/me", auth, async (req, res) => {
   res.send(req.user);
 });
