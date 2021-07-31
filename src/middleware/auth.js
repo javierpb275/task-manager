3e31193267b6d73de0bea46231da1d928d5b3343
javierpb275 <javierpb275@gmail.com>
2021-07-31 14:44:59 +0200

    Accepting Authentication Tokens: - set up middleware auth that we passed to  /users/me to get our profile. - auth middleware: 1. search for the header that the user must provide 2. then validates that header 3. find the associated user. 4. then we set property user of req to be equal to the user gotten. - now in the orute /users/me we just have respond with that req.user.

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,8 +3,14 @@ const User = require("../models/user");
 
 const auth = async (req, res, next) => {
     try {
-        const token = req.header('Authorization');
-        console.log(token);
+        const token = req.header('Authorization').replace('Bearer ', '');
+        const decoded = jwt.verify(token, 'thisisnodejs');
+        const user = await User.findOne({_id: decoded._id, 'tokens.token':token});
+        if (!user) {
+            throw new Error()
+        }
+        req.user = user;
+        next();
     } catch(e) {
         res.status(401).send({error: 'Please authenticate'})
     }
