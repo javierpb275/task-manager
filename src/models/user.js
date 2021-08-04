42457874a4c64eba066fe79dcbc2350b20aea63a
javierpb275 <javierpb275@gmail.com>
2021-08-04 21:45:41 +0200

    delete all tasks when deleting user

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const Task = require("./task");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -93,6 +94,13 @@ userSchema.pre("save", async function (next) {
   }
   next();
 });
+
+//delete user tasks when user is deleted:
+userSchema.pre('remove', async function (next) {
+  const user = this;
+  await Task.deleteMany({owner: user._id})
+  next()
+})
 
 const User = mongoose.model("User", userSchema);
 
