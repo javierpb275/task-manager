be92d98c5a8d12c5c6144c316cc90de68892d7c0
javierpb275 <javierpb275@gmail.com>
2021-08-04 20:16:38 +0200

    The UserTask Relationship: -added owner property to model Task which is the id of the user. -added auth middleware in create task and used spread operator to pass ...req.body and also pass the owner porperty which is the user.id of the current user that is logged in.

--- src/models/task.js
+++ src/models/task.js
@@ -10,6 +10,10 @@ const Task = mongoose.model("Task", {
     type: Boolean,
     default: false,
   },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true
+  }
 });
 
 module.exports = Task;
