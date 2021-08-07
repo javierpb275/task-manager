d418411f630374e242aac334399898dd4e92455f
javierpb275 <javierpb275@gmail.com>
2021-08-07 13:14:27 +0200

    Handling Express Errors: handling errors in /upload and /users/me/avatar by providing a second argument which is a function that reponse with the error message from the configuration in the multer middleware provided when sth went wrong.

--- src/index.js
+++ src/index.js
@@ -28,10 +28,7 @@ const upload = multer({
   }
 })
 
-const errorMiddleware = (req, res, next) => {
-  throw new Error('From my middleware');
-}
-app.post('/upload', errorMiddleware, (req, res) => {
+app.post('/upload', upload.single('upload'), (req, res) => {
   res.send();
 }, (error, req, res, next) => {
   res.status(400).send({error: error.message});
