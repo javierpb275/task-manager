618dc8a7638fd0b4d68eb8da9d8d21d069b89f19
javierpb275 <javierpb275@gmail.com>
2021-08-07 13:59:52 +0200

    Addign Images to User Profile: - add new property to user called avatar which is type Buffer. - added auth middleware to the avatar route. - we say that the property avatar of user is equal to the buffer obtained from the file. -then we save that user.

--- src/routers/user.js
+++ src/routers/user.js
@@ -86,7 +86,6 @@ router.delete("/users/me", auth, async (req, res) => {
 });
 
 const upload = multer({
-  dest: 'avatars',
   limits: {
     fileSize: 1000000 //measured in bytes. 1 megabyte: 1 million bytes
   },
@@ -99,7 +98,9 @@ const upload = multer({
   }
 })
 
-router.post("/users/me/avatar", upload.single('avatar'), (req, res) => {
+router.post("/users/me/avatar", auth, upload.single('avatar'), async (req, res) => {
+  req.user.avatar = req.file.buffer
+  await req.user.save();
   res.send();
 }, (error, req, res, next) => {
   res.status(400).send({error: error.message})
