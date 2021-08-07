4e24bf85522cd5575959acfa554179103b02a861
javierpb275 <javierpb275@gmail.com>
2021-08-07 17:25:15 +0200

    Auto-Cropping  and Image Formatting:
    used sharp package to configure the image uploaded to set the size and the type of file.

--- src/routers/user.js
+++ src/routers/user.js
@@ -3,6 +3,7 @@ const User = require("../models/user");
 const auth = require("../middleware/auth");
 const router = new express.Router();
 const multer = require('multer');
+const sharp = require('sharp');
 
 router.post("/users", async (req, res) => {
   const user = new User(req.body);
@@ -99,7 +100,8 @@ const upload = multer({
 })
 
 router.post("/users/me/avatar", auth, upload.single('avatar'), async (req, res) => {
-  req.user.avatar = req.file.buffer
+  const buffer = await sharp(req.file.buffer).resize({width: 250, height: 250}).png().toBuffer();
+  req.user.avatar = buffer;
   await req.user.save();
   res.send();
 }, (error, req, res, next) => {
@@ -118,7 +120,7 @@ router.get("/users/:id/avatar", async (req, res) => {
     if (!user || !user.avatar) {
       throw new Error();
     }
-    res.set("Content-Type", "image/jpg");
+    res.set("Content-Type", "image/png");
     res.send(user.avatar);
   } catch(e) {
     res.status(404).send();
