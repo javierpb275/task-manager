10d5ecfc1377efd7e4750ee4f6669615aa706d67
javierpb275 <javierpb275@gmail.com>
2021-08-07 13:01:21 +0200

    Handling Express Errors: using a custom errorMiddleware to see how we manage the errors

--- src/index.js
+++ src/index.js
@@ -27,8 +27,14 @@ const upload = multer({
     cb(undefined, false) */
   }
 })
-app.post('/upload', upload.single('upload'), (req, res) => {
+
+const errorMiddleware = (req, res, next) => {
+  throw new Error('From my middleware');
+}
+app.post('/upload', errorMiddleware, (req, res) => {
   res.send();
+}, (error, req, res, next) => {
+  res.status(400).send({error: error.message});
 })
 
 app.use(express.json(), userRouter, taskRouter);
