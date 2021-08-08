956a22ca3632fe2d7b38f01a53c8ce9b34cf4286
javierpb275 <javierpb275@gmail.com>
2021-08-08 12:50:51 +0200

    Exploring SendGrid: - set config of sendgrid checking if it works

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,1 +1,9 @@
-const sendgridAPIKey = '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'
+const sgMail = require('@sendgrid/mail');
+const sendgridAPIKey = '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'
+sgMail.setApiKey(sendgridAPIKey);
+sgMail.send({
+    to: 'javierpb275@gmail.com',
+    from: 'javierpb275@gmail.com',
+    subject: 'Checking sendgrid api works',
+    text: 'Helloooo!!!'
+});
