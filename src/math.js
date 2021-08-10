1e63a0a1de4fac1801165b6c0547087369a8f1a7
javierpb275 <javierpb275@gmail.com>
2021-08-10 18:40:56 +0200

    writing tests and assertions 2

--- src/math.js
+++ src/math.js
@@ -1,7 +1,4 @@
-const calculateTip = (total, tipPercent) => {
-    const tip = total * tipPercent;
-    return total + tip;
-}
+const calculateTip = (total, tipPercent = .25) => total + (total * tipPercent);
 
 module.exports = {
     calculateTip
--- tests/math.test.js
+++ tests/math.test.js
@@ -2,18 +2,10 @@ const {calculateTip} = require('../src/math');
 
 test('should calculate total with tip', () => {
     const total = calculateTip(10, .3);
-    if (total !== 13) {
-        throw new Error(`Total should be 13. Got ${total}`);
-    }
+    expect(total).toBe(13);
 })
 
-//
-//Why test?
-//
-//-Saves time
-// - creates reliable software
-//- gives flexibility to developers
-// -refactoring
-// -collaborating
-// -profiling
-// - Peace of mind
+test('should calculate total with default tip', () => {
+    const total = calculateTip(10);
+    expect(total).toBe(12.5);
+})
