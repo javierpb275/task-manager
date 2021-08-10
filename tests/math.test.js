d81984f40f0db46360463e070c675298964433b6
javierpb275 <javierpb275@gmail.com>
2021-08-10 17:14:26 +0200

    added test to scripts that runs jest. created directory tests where we put all our test. creates math.test.js where we checked how jest works using test()

---
+++ tests/math.test.js
@@ -0,0 +1,16 @@
+test("Hello world!", () => {});
+
+test("This should fail", () => {
+  throw new Error("Failure!");
+});
+
+//
+//Why test?
+//
+//-Saves time
+// - creates reliable software
+//- gives flexibility to developers
+// -refactoring
+// -collaborating
+// -profiling
+// - Peace of mind
