628eb3c23e3554bbcd1941dfdafd0eb6bfb0a782
javierpb275 <javierpb275@gmail.com>
2021-08-10 19:05:25 +0200

    writing tests and assertions 3

--- tests/math.test.js
+++ tests/math.test.js
@@ -1,11 +1,21 @@
-const {calculateTip} = require('../src/math');
+const {calculateTip, fahrenheitToCelsius, celsiusToFahrenheit} = require('../src/math');
 
 test('should calculate total with tip', () => {
     const total = calculateTip(10, .3);
     expect(total).toBe(13);
-})
+});
 
 test('should calculate total with default tip', () => {
     const total = calculateTip(10);
     expect(total).toBe(12.5);
-})
+});
+
+test('should convert 32 F to 0 C', () => {
+    const temp = fahrenheitToCelsius(32);
+    expect(temp).toBe(0);
+});
+
+test('should convert 0 C to 32 F', () => {
+    const temp = celsiusToFahrenheit(0);
+    expect(temp).toBe(32);
+});
