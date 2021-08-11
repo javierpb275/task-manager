803b4afa580edf14b3f5261dfa211a949c14654b
javierpb275 <javierpb275@gmail.com>
2021-08-11 19:13:26 +0200

    Testing an Express application: - testing create user route.

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,1 +1,10 @@
+const request = require('supertest');
+const app = require('../src/app');
 
+test('Should signup a new user', async () => {
+    await request(app).post('/users').send({
+        name: 'paco',
+        email: 'paco@gmail.com',
+        password: 'paco123'
+    }).expect(201)
+})
