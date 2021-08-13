08ccb6c352dd11d929d6d0340f9959d0a38570ac
javierpb275 <javierpb275@gmail.com>
2021-08-13 18:13:46 +0200

    testing with authentication with delete routes for user

--- tests/user.test.js
+++ tests/user.test.js
@@ -59,3 +59,18 @@ test('Should not get profile for unauthenticated user', async () => {
     .send()
     .expect(401);
 });
+
+test('Should delete account for user', async () => {
+    await request(app)
+    .delete('/users/me')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200);
+});
+
+test('Should not delete account for unauthenticated user', async () => {
+    await request(app)
+    .delete('/users/me')
+    .send()
+    .expect(401);
+});
