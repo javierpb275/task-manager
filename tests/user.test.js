3b262d2b14fbcfc33af7ef272a7bc9065e9cbd40
javierpb275 <javierpb275@gmail.com>
2021-08-13 18:18:01 +0200

    formatted files

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,76 +1,81 @@
-const request = require('supertest');
-const jwt = require('jsonwebtoken');
-const mongoose = require('mongoose');
-const app = require('../src/app');
-const User = require('../src/models/user');
+const request = require("supertest");
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
+const app = require("../src/app");
+const User = require("../src/models/user");
 
 const userOneId = new mongoose.Types.ObjectId();
 
 const userOne = {
-    _id: userOneId,
-    name: 'Mike',
-    email: 'mike@example.com',
-    password: '56what!!',
-    tokens: [{
-        token: jwt.sign({_id: userOneId}, process.env.JWT_SECRET)
-    }]
-}
+  _id: userOneId,
+  name: "Mike",
+  email: "mike@example.com",
+  password: "56what!!",
+  tokens: [
+    {
+      token: jwt.sign({ _id: userOneId }, process.env.JWT_SECRET),
+    },
+  ],
+};
 
 beforeEach(async () => {
-    await User.deleteMany();
-    await new User(userOne).save();
+  await User.deleteMany();
+  await new User(userOne).save();
 });
 
 //afterEach(() => {})
 
-test('Should signup a new user', async () => {
-    await request(app).post('/users').send({
-        name: 'mery',
-        email: 'mery@gmail.com',
-        password: 'mery123'
-    }).expect(201)
+test("Should signup a new user", async () => {
+  await request(app)
+    .post("/users")
+    .send({
+      name: "mery",
+      email: "mery@gmail.com",
+      password: "mery123",
+    })
+    .expect(201);
 });
 
-test('Should login existing user', async () => {
-    await request(app).post('/users/login').send({
-        email: userOne.email,
-        password: userOne.password
-    }).expect(200)
-})
+test("Should login existing user", async () => {
+  await request(app)
+    .post("/users/login")
+    .send({
+      email: userOne.email,
+      password: userOne.password,
+    })
+    .expect(200);
+});
 
-test('Should not login nonexisting user', async () => {
-    await request(app).post('/users/login').send({
-        email: 'hello@example.com',
-        password: 'hello123'
-    }).expect(400)
-})
+test("Should not login nonexisting user", async () => {
+  await request(app)
+    .post("/users/login")
+    .send({
+      email: "hello@example.com",
+      password: "hello123",
+    })
+    .expect(400);
+});
 
-test('Should get profile for user', async () => {
-    await request(app)
-    .get('/users/me')
-    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+test("Should get profile for user", async () => {
+  await request(app)
+    .get("/users/me")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
     .send()
     .expect(200);
 });
 
-test('Should not get profile for unauthenticated user', async () => {
-    await request(app)
-    .get('/users/me')
-    .send()
-    .expect(401);
+test("Should not get profile for unauthenticated user", async () => {
+  await request(app).get("/users/me").send().expect(401);
 });
 
-test('Should delete account for user', async () => {
-    await request(app)
-    .delete('/users/me')
-    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+test("Should delete account for user", async () => {
+  await request(app)
+    .delete("/users/me")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
     .send()
     .expect(200);
 });
 
-test('Should not delete account for unauthenticated user', async () => {
-    await request(app)
-    .delete('/users/me')
-    .send()
-    .expect(401);
+test("Should not delete account for unauthenticated user", async () => {
+  await request(app).delete("/users/me").send().expect(401);
 });
